f24c5380d43373ce0e5d519b81319f6216958539
V-Javier-VT <vjvergara@uce.edu.ec>
2025-02-13 22:32:47 -0500

    user services ready

--- BACKEND/services/user-service/get-all-users-service/src/index.js
+++ BACKEND/services/user-service/get-all-users-service/src/index.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const userRoutes = require('./src/routes/userRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/get-user-service/users', userRoutes);
+app.use('/get-all-users-service/users', userRoutes);
 
-const PORT = process.env.PORT || 5008;
+const PORT = process.env.PORT || 5011;
 app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
